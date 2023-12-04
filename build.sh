53a8fffacc3c2410f8430d26cc6776aacca07fb8
lvxiangxiang <lvxiangxiang@shoplineapp.com>
2023-12-04 21:44:34 +0800

    ~

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+
+python main.py
+
+
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.25/mdbook-v0.4.25-x86_64-unknown-linux-gnu.tar.gz | tar xvz && ./mdbook build
